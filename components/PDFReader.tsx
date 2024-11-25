e3363c5dbbbccaf751a57239f29e85dda6d607ca
Ermiyas Kerebih <ermiyask18@gmail.com>
2024-11-25 22:16:59 +0300

    fix import error

--- components/PDFReader.tsx
+++ components/PDFReader.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import PDFViewer from "pdf-viewer-reactjs";
 
-const PDFReader = ({ url }: { url: string }) => {
-  return (
-    <PDFViewer
-      document={{
-        url: url,
-      }}
-    />
-  );
+const PDFReader = () => {
+  return <div>PDFReader</div>;
 };
+
 export default PDFReader;
+
+// import React from "react";
+// import PDFViewer from "pdf-viewer-reactjs";
+
+// const PDFReader = ({ url }: { url: string }) => {
+//   return (
+//     <PDFViewer
+//       document={{
+//         url: url,
+//       }}
+//     />
+//   );
+// };
+// export default PDFReader;
