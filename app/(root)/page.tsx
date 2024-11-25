e3a99b86ae65cc01955f09f4665bd1f01919f77a
Ermiyas Kerebih <ermiyask18@gmail.com>
2024-11-25 22:20:00 +0300

    fix code error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,7 @@ import Books from "@/components/Books";
 import Hero from "@/components/Hero";
 import React from "react";
 
-interface Props {
-  searchParams: { [key: string]: string | undefined };
-}
-
-const Home = ({ searchParams }: Props) => {
+const Home = () => {
   return (
     <main className="w-full">
       <Hero />
